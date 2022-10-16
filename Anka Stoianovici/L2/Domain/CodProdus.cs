12f084828461c9f55339433dca4952623ddc7c12
Anka-Snezana Stoianovici <61665120+ankastoianovici@users.noreply.github.com>
2022-10-16 15:01:26 +0300

    Update CodProdus.cs

--- Anka Stoianovici/L2/Domain/CodProdus.cs
+++ Anka Stoianovici/L2/Domain/CodProdus.cs
@@ -1,41 +1,8 @@
-/*using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using System.Linq;
-
-namespace L2.Domain
-{
-    public record CodProdus
-    {
-        public decimal Value { get; }
-        private CodProdus(decimal value)
-        {
-            if(value>0 && value<=999)
-            {
-                Value=value;
-            }
-            else
-            {
-                throw new InvalidCodProdus($"{value:0.##} is an invalid grade value.");
-            }
-        }
-        public CodProdus Round()
-        {
-            var roundedValue=Math.Round(Value);
-            return new CodProdus(roundedValue);
-        }
-        public override string ToString()
-        {
-            return $"{Value:0.##}";
-        }
-    }
-}*/
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Text;
+using System.Linq;
 
 namespace L2.Domain
 {
